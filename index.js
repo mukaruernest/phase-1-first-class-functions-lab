bbf667b941fd4eb631fb141efffb33a5c12c4509
mukaruernest <mukaruernesto@gmail.com>
2023-01-13 12:06:18 +0300

    First class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+//Code your solution in this file!
+const returnFirstTwoDrivers = (function (drivers) {
+    return drivers.slice(0, 2)
+})
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = (function (scoberDrivers) {
+    return scoberDrivers.slice(-2)
+})
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+// const fare = createFareMultiplier(3)
+// console.log(fare(5))
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, lastFirstDrivers) {
+    return lastFirstDrivers(drivers)
+}
+
+
+
+
+
+
